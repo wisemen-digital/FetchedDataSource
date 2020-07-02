37d02ce0d9bcbb26cc532b3e2844bbc2ac9d1688
David Jennes <david.jennes@gmail.com>
2020-07-02 23:42:09 +0200

    Deprecate factory functions, instead use constructor directly

--- Source/FetchedDataSource+Factory.swift
+++ Source/FetchedDataSource+Factory.swift
@@ -10,10 +10,12 @@
 import UIKit
 
 public enum FetchedDataSource {
+	@available(*, deprecated, renamed: "FetchedTableDataSource.init(controller:view:delegate:animateChanges:)", message: "Use FetchedTableDataSource directly")
 	public static func `for`<ResultType: NSFetchRequestResult>(tableView: UITableView, controller: NSFetchedResultsController<ResultType>, delegate: FetchedTableDataSourceDelegate, animateChanges: Bool = true) -> FetchedTableDataSource<ResultType> {
 		return FetchedTableDataSource(controller: controller, view: tableView, delegate: delegate, animateChanges: animateChanges)
 	}
 
+	@available(*, deprecated, renamed: "FetchedCollectionDataSource.init(controller:view:delegate:animateChanges:)", message: "Use FetchedCollectionDataSource directly")
 	public static func `for`<ResultType: NSFetchRequestResult>(collectionView: UICollectionView, controller: NSFetchedResultsController<ResultType>, delegate: FetchedCollectionDataSourceDelegate, animateChanges: Bool = true) -> FetchedCollectionDataSource<ResultType> {
 		return FetchedCollectionDataSource(controller: controller, view: collectionView, delegate: delegate, animateChanges: animateChanges)
 	}
